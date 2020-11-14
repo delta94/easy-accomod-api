39ac4f25665ab4695e1f122c2313a6a3851b78a2
tranducminh <minhscdhpvn@gmail.com>
2020-11-14 19:23:40 +0700

    Init Project and config firebase

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import * as dotenv from 'dotenv'
+import express, {Response, NextFunction} from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import compression from 'compression'
+import bodyParser from 'body-parser'
+import hpp from 'hpp'
+import mongoose from 'mongoose'
+
+dotenv.config()
+
+const PORT: number = parseInt(process.env.PORT as string, 10) || 5000
+const {DB_USERNAME, DB_PASSWORD, DB_NAME} = process.env
+
+const MONGODB_URI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.3ukly.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+
+mongoose
+    .connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(() => console.log('Database connected ...'))
+    .catch((error) => console.log('Could not connect to database ...', error.message))
+
+const app = express()
+
+app.use(cors())
+app.use(helmet())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(compression())
+app.use(hpp())
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
